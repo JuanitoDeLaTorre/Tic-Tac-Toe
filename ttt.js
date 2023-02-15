f3fc8a1abd8f1db58184f9ecc115ebee2a477c4a
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-02-15 16:29:20 -0500

    working 3D array checking content of each cell

--- ttt.js
+++ ttt.js
@@ -4,10 +4,28 @@
 const currentPlayerHTML = document.querySelector('#currentTurn');
 // Other options is make the HTML have an id and use document.getElementById('currentTurn');
 // As good practice, COMMENT out console.log statements
-console.log(currentPlayerHTML);
 // querySelectorAll will ALWAYS grab ALL of the items. This always create an array that is grabbing every item
 const squares = document.querySelectorAll('.square');
-console.log(squares);
+const reset = document.querySelector(".reset")
+const check = document.querySelector(".check")
+const xWins = document.querySelector(".xWins")
+
+//select all block ID's
+let tl = document.querySelector("#tl")
+let tc = document.querySelector("#tc")
+let tr = document.querySelector("#tr")
+let ml = document.querySelector("#ml")
+let mc = document.querySelector("#mc")
+let mr = document.querySelector("#mr")
+let bl = document.querySelector("#bl")
+let bc = document.querySelector("#bc")
+let br = document.querySelector("#br")
+
+let squareValues = [[tl.innerText,tc.innerText,tr.innerText],[ml.innerText,mc.innerText,mr.innerText],[bl.innerText,bc.innerText,br.innerText]]
+
+
+
+
 // Initialize player
 let player;
 
@@ -15,7 +33,6 @@ let player;
 if(Math.random() > 0.5) {player = 'X'}
 else {player = "O"};
 
-console.log(player);
 
 
 // I put my functions in this spot 
@@ -39,6 +56,35 @@ for(let i = 0; i < squares.length; i++) {
         }
         updateTurnOnDOM();
     })
+}
+
+reset.addEventListener("click",()=> {
+    squares.forEach((square)=> {
+        square.innerHTML = ""
+    })
+})
+
+check.addEventListener("click",()=> {
+    // console.log(tl.innerText)
+    // console.log(mc.innerText)
+    // console.log(br.innerText)
+    if(tl.innerText === "X" && mc.innerText === "X" && br.innerText === "X") {
+        xWins.innerText = "X has 1 wins"
+    }
+    updateSquareValues();
+})
+
+function updateSquareValues() {
+    squareValues[0][0] = tl.innerText;
+    squareValues[0][1] = tc.innerText;
+    squareValues[0][2] = tr.innerText;
+    squareValues[1][0] = ml.innerText;
+    squareValues[1][1] = mc.innerText;
+    squareValues[1][2] = mr.innerText;
+    squareValues[2][0] = bl.innerText;
+    squareValues[2][1] = bc.innerText;
+    squareValues[2][2] = br.innerText;
+    console.log(squareValues)
 }
 
 // squares.forEach(square => {
