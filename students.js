12399be2dbf45381f0f48dbaf7f064e7ad58ae6a
JuanitoDeLaTorre <home.john.thomas@gmail.com>
2023-02-16 08:55:27 -0500

    deleted superfluous files

--- students.js
+++
@@ -1,5 +0,0 @@
-const students = [
-    "Ahmad", "Ali", "Brandon", "David", "Johnathan P", "John T", "Joshua", "Leo", "Luke", "Sean", "Wylie"
-];
-
-console.log(students[Math.floor(Math.random() * students.length)])
